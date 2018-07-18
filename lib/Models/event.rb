abcc3a373a892ec8f23fcd211d380b28666b22f2
Timur <hometim55@gmail.com>
2018-07-18 22:04:56 +0300

    Fix code style

--- lib/Models/event.rb
+++ lib/Models/event.rb
@@ -1,9 +1,11 @@
+# Simple event model
 class Event
 
   attr_accessor :id, :name, :done
 
   def initialize(name, done)
-	@name = name
-	@done = done
+    @name = name
+    @done = done
   end
+
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,32 +1,32 @@
-require_relative 'dbManager.rb'
-require_relative 'event.rb'
+require_relative 'Managers/dbManager.rb'
+require_relative 'Models/event'
 
-puts "Welcome to TODO LIST!"
+puts 'Welcome to TODO LIST!'
 
 db_manager = DbManager.new
 
-while true
-  puts "Input command: "
-    
+loop do
+  puts 'Input command: '
+
   command = gets.chomp
-    
-  if command[0, 5].include? "--add"
+
+  if command[0, 5].include? '--add'
     event_name = command[6, command.length - 1]
     event = Event.new event_name, false
     db_manager.create(event)
-    
-    elsif command == "--list"
-      db_manager.readAll
-    
-    elsif command[0, 6].include? "--done"
-      id = command[7, command.length - 1]
-      db_manager.done(id)
-    
-    elsif command[0, 8].include? "--delete"
-      id = command[9, command.length - 1]
-      db_manager.delete(id)
-    
-    else
-      print "Command #{command} not found (to see documentation: --help)\n"
-  end  
+
+  elsif command == '--list'
+    db_manager.read_all
+
+  elsif command[0, 6].include? '--done'
+    id = command[7, command.length - 1]
+    db_manager.done(id)
+
+  elsif command[0, 8].include? '--delete'
+    id = command[9, command.length - 1]
+    db_manager.delete(id)
+
+  else
+    print "Command #{command} not found (to see documentation: --help)\n"
+  end
 end
